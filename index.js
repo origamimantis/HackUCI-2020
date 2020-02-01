8aa0c3b33926a37e7e96a139ebae1f128c4f27f3
Bloxore <klinman.max@gmail.com>
2020-02-01 00:31:07 -0800

    log

--- index.js
+++ index.js
@@ -9,7 +9,8 @@ app.set("views", __dirname + "/views");
 
 
 app.get("/",  (req, res) => {
-  res.render("index");
+	console.log("access");
+	res.render("index");
 });
 
 app.listen(port, () => console.log("I work"));

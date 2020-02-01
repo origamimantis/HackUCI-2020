c6be9daec835a6df8270422f5d63e5af5593660d
eric zhang <ezhang323@gmail.com>
2020-02-01 01:07:58 -0800

    f -> F

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const port = 80;
 
 app.get("/",  (req, res) => {
 	console.log("access");
-	res.sendfile("views/index.html");
+	res.sendFile("views/index.html");
 });
 
 http.createServer(app).listen(port, () => console.log("I work"));

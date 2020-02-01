c249b32e252a3a717e735468b02375da3b4bcacb
eric zhang <ezhang323@gmail.com>
2020-02-01 00:24:18 -0800

    server tiny

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
+const express = require("express");
+
+const app = express();
+const port = 4141;
+
+
+
+app.set("views", __dirname + "/views");
+
+
+app.get("/",  (req, res) => {
+  res.render("index");
+});
+
 console.log("I work");

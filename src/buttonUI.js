7b45aa5f39f0d266ac9792ffef5921d3b8b90597
Your Name <you@example.com>
2020-02-02 01:59:23 +0000

    test

--- src/buttonUI.js
+++ src/buttonUI.js
@@ -41,5 +41,73 @@ class ToggleButton extends React.Component {
 	}
 }
 
+class IDField extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			id: "",
+		}
+	
+		document.addEventListener("onServConn", (e) => {
+			this.setState({id: e.detail.id});	
+		});
+	}
+
+	render() {
+		return (
+			<h1>Your id is: {this.state.id} </h1>
+		);
+	}
+}
+
+class PairForm extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {value: ''};
+		this.handleChange = this.handleChange.bind(this);
+		this.handleSubmit = this.handleSubmit.bind(this);
+
+		this.pairEvent = new CustomEvent('pairRequest', { detail: {id: "000000"} })
+		
+	}
+	
+	handleChange(event) {
+		this.setState({value: event.target.value});
+	}
+	
+	handleSubmit(event) {
+		event.preventDefault();
+		this.pairEvent.detail.id = this.state.value;
+		document.dispatchEvent(this.pairEvent);
+	}
+
+
+	render() {
+		return (
+			<form id = "pairform" onSubmit={this.handleSubmit}>
+				<input key = "1" name="id" type="text" value ={this.state.value} onChange={this.handleChange}></input>
+				<input key = "2" type="submit" value="Pair"></input>
+			</form>
+		);
+	}
+}
+
+class clearButton extends React.Component {
+	constructor(props){ 
+		super(props);
+	} 
+
+	render() {
+		return (
+			<button onClick={() => {
+
+			}}>
+				Clear
+			</button>
+
+		);
+	}
+}
+
 const domContainer = document.querySelector('#root');
-ReactDOM.render(<ToggleButton />, domContainer);
+ReactDOM.render([<ToggleButton />, <IDField />, <PairForm />], domContainer);
